389c76b4e6f94110140cf03aa5f431fc4e9a069d
Michael Richters <gedankenexperimenter@gmail.com>
2019-04-18 19:37:08 -0500

    Remove obsolete release delay constant
    Signed-off-by: Michael Richters <gedankenexperimenter@gmail.com>

--- src/qukeys/constants.h
+++ src/qukeys/constants.h
@@ -18,17 +18,5 @@ constexpr byte queue_max{8};
 // flushed before the timeout expires.
 constexpr uint16_t timeout{200};
 
-// For people who are in the habit of simultaneously releasing modifiers with the target
-// key (the one to be modified), a release delay can be set. This means that if the qukey
-// release is detected first, it won't be flushed from the queue immediately, but will
-// wait until this delay expires (in milliseconds) before flushing the qukey in its
-// primary state. If, before that happens, the target key's release is detected, it will
-// instead get flushed in its alternate state. This is a global default value that will
-// apply to all qukeys, but which can be overridden in each Qukey instance. Note that
-// setting this value too high (>20ms is not recommended) can result in unintended
-// alternate keycodes (i.e. modifiers) during rollover when typing, which is probably
-// worse than unintended primary keycodes.
-constexpr byte qukey_release_delay{0};
-
 } // namespace qukeys {
 } // namespace kaleidoglyph {
